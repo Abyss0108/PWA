0f33bfabeaf23141021a42163a4fba7a08d74c85
Abyss0108 <147667703+Abyss0108@users.noreply.github.com>
2024-10-24 13:06:07 -0600

    Cambios en el Service-Worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+<<<<<<< Updated upstream
 //import * as serviceWorkerRegistration from './ServiceWorkerRegistration'; 
+=======
+>>>>>>> Stashed changes
 
 const root = document.getElementById('root');
 
@@ -16,6 +19,21 @@ ReactDOM.render(
   root
 );
 
+<<<<<<< Updated upstream
+=======
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado con éxito:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Error al registrar el Service Worker:', error);
+      });
+  });
+}
+
+>>>>>>> Stashed changes
 
 
 reportWebVitals();
